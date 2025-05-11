4a5cca11850d5750be17f35076abfd41bdc0c54e
Tai7sy <12840640+Tai7sy@users.noreply.github.com>
2025-05-11 19:17:43 +0800

    Fix candles data

--- hummingbot/data_feed/candles_feed/gate_io_spot_candles/gate_io_spot_candles.py
+++ hummingbot/data_feed/candles_feed/gate_io_spot_candles/gate_io_spot_candles.py
@@ -1,5 +1,6 @@
 import logging
 import time
+import random
 from typing import List, Optional
 
 from hummingbot.core.network_iterator import NetworkStatus
@@ -35,10 +36,19 @@ class GateioSpotCandles(CandlesBase):
     @property
     def health_check_url(self):
         return self.rest_url + CONSTANTS.HEALTH_CHECK_ENDPOINT
+    
+    @property
+    def is_web_api(self):
+        return "XMR_" in self._ex_trading_pair
 
     @property
     def candles_url(self):
+        # fix hidden pair
+        if self.is_web_api:
+            return "https://www.gate.io/apiw/v2/spot/candlesticks"
+
         return self.rest_url + CONSTANTS.CANDLES_ENDPOINT
+
 
     @property
     def candles_endpoint(self):
@@ -90,29 +100,105 @@ class GateioSpotCandles(CandlesBase):
             "currency_pair": self._ex_trading_pair,
             "interval": self.interval,
             "from": start_time,
-            "to": end_time
+            "to": end_time,
+            "limit": limit,
         }
 
     def _parse_rest_candles(self, data: dict, end_time: Optional[int] = None) -> List[List[float]]:
         new_hb_candles = []
-        for i in data:
-            timestamp = self.ensure_timestamp_in_seconds(i[0])
-            open = i[5]
-            high = i[3]
-            low = i[4]
-            close = i[2]
-            volume = i[6]
-            quote_asset_volume = i[1]
-            # no data field
-            n_trades = 0
-            taker_buy_base_volume = 0
-            taker_buy_quote_volume = 0
-            new_hb_candles.append([timestamp, open, high, low, close, volume,
-                                   quote_asset_volume, n_trades, taker_buy_base_volume,
-                                   taker_buy_quote_volume])
+
+        if self.is_web_api:
+            """
+            {
+                "timestamp": 1746957278105,
+                "method": "/apiw/v2/spot/candlesticks",
+                "code": 200,
+                "message": "Success",
+                "page": null,
+                "limit": null,
+                "total": null,
+                "data": [
+                    {
+                        "t": 1746950400,
+                        "v": "252.48130000",
+                        "c": "325.5",
+                        "h": "325.5",
+                        "l": "320.99",
+                        "o": "320.99",
+                        "sum": "81889.93622900"
+                    },
+                    {
+                        "t": 1746954000,
+                        "v": "715.76580000",
+                        "c": "328.36",
+                        "h": "329.84",
+                        "l": "325.49",
+                        "o": "325.5",
+                        "sum": "234296.35352300"
+                    }
+                ]
+            }
+            """
+            for candle in data["data"]:
+                timestamp = self.ensure_timestamp_in_seconds(candle["t"])
+                open = candle["o"]
+                high = candle["h"]
+                low = candle["l"]
+                close = candle["c"]
+                volume = candle["v"]
+                quote_asset_volume = candle["sum"]
+                n_trades = 0
+                taker_buy_base_volume = 0
+                taker_buy_quote_volume = 0
+                new_hb_candles.append([
+                    timestamp, open, high, low, close, volume,
+                    quote_asset_volume, n_trades, taker_buy_base_volume, taker_buy_quote_volume
+                ])
+        else:
+            """
+            [
+                [
+                    "1743350400",
+                    "4350376.86757900",
+                    "82294.6",
+                    "82811.7",
+                    "82166.7",
+                    "82727.9",
+                    "52.72269000",
+                    "true"
+                ]
+            ]
+            """
+            for i in data:
+                timestamp = self.ensure_timestamp_in_seconds(i[0])
+                open = i[5]
+                high = i[3]
+                low = i[4]
+                close = i[2]
+                volume = i[6]
+                quote_asset_volume = i[1]
+                # no data field
+                n_trades = 0
+                taker_buy_base_volume = 0
+                taker_buy_quote_volume = 0
+                new_hb_candles.append([timestamp, open, high, low, close, volume,
+                                       quote_asset_volume, n_trades, taker_buy_base_volume,
+                                       taker_buy_quote_volume])
         return new_hb_candles
 
     def ws_subscription_payload(self):
+        
+        # fix hidden pair
+        if self.is_web_api:
+            """
+            {"id":1777424,"method":"kline.subscribe","params":[["XMR_USDT",60]]}
+            """
+            return {
+                "id": random.randint(1000000, 9999999), # random id
+                "method": "kline.subscribe",
+                "params": [[self._ex_trading_pair, self.interval]]
+            }
+        
         return {
             "time": int(self._time()),
             "channel": CONSTANTS.WS_CANDLES_ENDPOINT,
@@ -130,6 +216,42 @@ class GateioSpotCandles(CandlesBase):
             candles_row_dict["close"] = data["result"]["c"]
             candles_row_dict["volume"] = data["result"]["a"]
             candles_row_dict["quote_asset_volume"] = data["result"]["v"]
+            candles_row_dict["n_trades"] = 0
+            candles_row_dict["taker_buy_base_volume"] = 0
+            candles_row_dict["taker_buy_quote_volume"] = 0
+            return candles_row_dict
+
+        # web api
+        """
+        {
+          "method": "kline.update",
+          "params": [
+            [
+              1746957900,
+              "330.51",
+              "330.51",
+              "330.51",
+              "330.51",
+              "0",
+              "0",
+              "XMR_USDT",
+              false,
+              "1m"
+            ]
+          ],
+          "id": null,
+          "v": 3
+        }
+        """
+        if data.get("method") == "kline.update" and len(data["params"]) > 0:
+            params = data["params"][0]
+            candles_row_dict["timestamp"] = self.ensure_timestamp_in_seconds(params[0])
+            candles_row_dict["open"] = params[1]
+            candles_row_dict["high"] = params[2]
+            candles_row_dict["low"] = params[4]
+            candles_row_dict["close"] = params[3]
+            candles_row_dict["volume"] = params[5]
+            candles_row_dict["quote_asset_volume"] = params[6]
             candles_row_dict["n_trades"] = 0
             candles_row_dict["taker_buy_base_volume"] = 0
             candles_row_dict["taker_buy_quote_volume"] = 0
